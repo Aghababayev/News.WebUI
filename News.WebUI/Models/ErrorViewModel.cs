150ed0ffcf4d9de0fcff6a277681b627bf430cd6
turxaymammadov <109957504+turxaymammadov@users.noreply.github.com>
2023-08-18 13:48:47 +0400

    Add project files.

---
+++ News.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace News.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
