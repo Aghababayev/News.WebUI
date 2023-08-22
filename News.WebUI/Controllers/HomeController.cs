d3902191f46e5e574974f70aa0bf00c5b77a4449
Aghababayev <109957504+turxaymammadov@users.noreply.github.com>
2023-08-23 03:46:38 +0400

    Add viewcomponent for content of news

--- News.WebUI/Controllers/HomeController.cs
+++ News.WebUI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using News.WebUI.Application.Reader_Module;
+using News.WebUI.Application.Visitor_Module;
 using News.WebUI.Models;
 using System;
 using System.Collections.Generic;
@@ -15,13 +16,11 @@ namespace News.WebUI.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly IMediator _mediator;
    
-        public HomeController(ILogger<HomeController> logger, IMediator mediator)
+        public HomeController(IMediator mediator)
         {
-            _mediator = mediator;
-            _logger = logger;   
+            _mediator = mediator;     
         }
 
         public  async Task<IActionResult> Index()
@@ -30,7 +29,12 @@ namespace News.WebUI.Controllers
             return View(values);
         }
 
-       public async Task<IActionResult> GetByContentId()
+        public async Task<IActionResult> GetByInformatioId(int id)
+        {
+            var value = await _mediator.Send(new GetByInformatioIdQuerry { InformationID = id });
+            return View(value);
+        }
+        public async Task<IActionResult> GetValidContents()
         {
             var values = await _mediator.Send(new GetContentIdByActiveNewsQuerry());
             return null;
---
+++ News.WebUI/ViewComponents/LayoutActiveContentViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using News.WebUI.Application.Reader_Module;
+using System.Threading.Tasks;
+
+namespace News.WebUI.ViewComponents
+{
+    public class LayoutActiveContentViewComponent : ViewComponent
+    {
+        private readonly IMediator _mediator;
+        public LayoutActiveContentViewComponent(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _mediator.Send(new GetContentIdByActiveNewsQuerry());
+            return View(values);
+        }
+
+    }
+}
